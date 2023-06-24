df01a5bd428dc93ec94d1e2bea65a47e905cc329
baekseungchan1117 <bsc7386@gmail.com>
2023-06-24 16:49:11 +0900

    메인 화면

---
+++ js/main_page_scroll.js
@@ -0,0 +1,24 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("show"); 
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].classList.add("show"); 
+
+  setTimeout(showSlides, 3000); // Change the slide every 3 seconds (3000 milliseconds)
+}
+
+
+
+
