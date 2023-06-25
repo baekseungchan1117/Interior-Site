21e8833130a110df3f75ccc5f6837c213ba1e0fc
baekseungchan1117 <bsc7386@gmail.com>
2023-06-25 15:31:36 +0900

    프로젝트 페이지 캐러셀 제작

--- js/project_page_scroll.js
+++ js/project_page_scroll.js
@@ -1,34 +1,87 @@
-const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
-const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
-let currentIdx = 0; //현재 슬라이드 index
-const slideCount = slideImg.length; // 슬라이드 개수
-const prev = document.querySelector('.prev'); //이전 버튼
-const next = document.querySelector('.next'); //다음 버튼
-const slideWidth = 300; //한개의 슬라이드 넓이
-const slideMargin = 100; //슬라이드간의 margin 값
+;(function () {
+  'use strict'
 
-//전체 슬라이드 컨테이너 넓이 설정
-slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
-
-function moveSlide(num) {
-  slides.style.left = -num * 100 + 'px';
-  currentIdx = num;
-}
-
-prev.addEventListener('click', function () {
-  /*첫 번째 슬라이드로 표시 됐을때는 
-  이전 버튼 눌러도 아무런 반응 없게 하기 위해 
-  currentIdx !==0일때만 moveSlide 함수 불러옴 */
-
-  if (currentIdx !== 0) moveSlide(currentIdx - 1);
-});
-
-next.addEventListener('click', function () {
-  /* 마지막 슬라이드로 표시 됐을때는 
-  다음 버튼 눌러도 아무런 반응 없게 하기 위해
-  currentIdx !==slideCount - 1 일때만 
-  moveSlide 함수 불러옴 */
-  if (currentIdx !== slideCount - 1) {
-    moveSlide(currentIdx + 1);
+  const get = (target) => {
+    return document.querySelector(target)
   }
-});
+
+  class Carousel {
+    constructor(caruoselElement) {
+      this.caruoselElement = caruoselElement
+      this.itemClassName = 'carousel_item'
+      this.items = this.caruoselElement.querySelectorAll('.carousel_item')
+      this.totalItems = this.items.length
+      this.current = 0
+    }
+
+    initCarousel() {
+      this.items[this.totalItems - 1].classList.add('prev')
+      this.items[0].classList.add('active')
+      this.items[1].classList.add('next')
+    }
+
+    moveCarouselTo() {
+      let prev = this.current - 1
+      let next = this.current + 1
+
+      if (this.current === 0) {
+        prev = this.totalItems - 1
+      } else if (this.current === this.totalItems - 1) {
+        next = 0
+      }
+
+      for (let i = 0; i < this.totalItems; i++) {
+        if (i === this.current) {
+          this.items[i].className = this.itemClassName + ' active'
+        } else if (i === prev) {
+          this.items[i].className = this.itemClassName + ' prev'
+        } else if (i === next) {
+          this.items[i].className = this.itemClassName + ' next'
+        } else {
+          this.items[i].className = this.itemClassName
+        }
+      }
+    }
+
+    movePrev() {
+      if (this.current === 0) {
+        this.current = this.totalItems - 1
+      } else {
+        this.current--
+      }
+      this.moveCarouselTo()
+    }
+
+    moveNext() {
+      if (this.current === this.totalItems - 1) {
+        this.current = 0
+      } else {
+        this.current++
+      }
+      this.moveCarouselTo()
+    }
+
+    setEventListeners() {
+      this.prevButton = this.caruoselElement.querySelector(
+        '.carousel_button--prev'
+      )
+      this.nextButton = this.caruoselElement.querySelector(
+        '.carousel_button--next'
+      )
+
+      this.prevButton.addEventListener('click', () => {
+        this.movePrev()
+      })
+      this.nextButton.addEventListener('click', () => {
+        this.moveNext()
+      })
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const carouselElement = get('.carousel')
+    const carousel = new Carousel(carouselElement)
+    carousel.initCarousel()
+    carousel.setEventListeners()
+  })
+})()
